218ea98152cde65c5cc3e39d8e7f518f7693513d
pytherik <erikberndt@gmx.net>
2023-04-03 21:50:54 +0200

    rearrange indentation

--- FIAE_PHP_Parralel/server.py
+++ FIAE_PHP_Parralel/server.py
@@ -6,12 +6,12 @@ PORT = 9999
 
 class NeuralHTTP(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/html')
-    self.end_headers()
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-    self.wfile.write(bytes('<html><body><h1>HALLOOOOOOO</h1></body></html>', 'utf-8'))
+        self.wfile.write(bytes('<html><body><h1>HALLOOOOOOO</h1></body></html>', 'utf-8'))
 
 
 server = HTTPServer((HOST, PORT), NeuralHTTP)
