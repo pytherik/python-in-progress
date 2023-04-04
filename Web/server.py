a40b27159f11dd0e1a093c000b2520b4325ca2a6
pytherik <erikberndt@gmx.net>
2023-04-04 21:26:22 +0200

    update server

--- Web/server.py
+++ Web/server.py
@@ -4,12 +4,13 @@ from config import HOST, PORT
 
 class NeuralHTTP(BaseHTTPRequestHandler):
 
+    content = "HALLOOOO WEELLLTTTT"
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        self.wfile.write(bytes('<html><body><h1>HALLOOOOOOO</h1></body></html>', 'utf-8'))
+        self.wfile.write(bytes(f'<html><body><h1>{self.content}</h1></body></html>', 'utf-8'))
 
 
 server = HTTPServer((HOST, PORT), NeuralHTTP)
